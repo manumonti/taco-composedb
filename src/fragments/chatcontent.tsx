2e82d860840a9a33fbe548d742979296ebd69766
Piotr Roslaniec <p.roslaniec@gmail.com>
2024-04-22 14:34:28 +0200

    feature: add basic error handling during decryption

--- src/fragments/chatcontent.tsx
+++ src/fragments/chatcontent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Message } from "../../types";
 import {} from "../hooks/messages-transform.types";
 import Avatar from "./avatar";
@@ -10,24 +10,32 @@ interface ChatContentProps {
 }
 
 const ChatContent = ({ messages }: ChatContentProps) => {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const handleDecrypt = async (event: any, message: Message) => {
     const mkb64 = message.ciphertext;
     const mkBytes = await decodeb64(mkb64);
     const thresholdMessageKit = ThresholdMessageKit.fromBytes(mkBytes);
-
-    const decryptedMessageBytes = await decryptWithTACo(
-      thresholdMessageKit,
-      domains.TESTNET,
-    );
-    const decryptedMessage = new TextDecoder().decode(decryptedMessageBytes)
+    let decryptedMessageBytes;
+    try {
+      decryptedMessageBytes = await decryptWithTACo(
+          thresholdMessageKit,
+          domains.TESTNET,
+      );
+    } catch (code) {
+      console.error(`Error decrypting message: ${code}`);
+      setErrorMessage(`Error decrypting message: ${code}`);
+      return;
+    }
+    const decryptedMessage = new TextDecoder().decode(decryptedMessageBytes);
     event.target.parentElement.children[1].innerText = decryptedMessage;
-    event.target.innerText = "Decoded!"
+    event.target.innerText = "Decoded!";
   }
-
 
   return (
     <div className="max-h-100 h-80 px-6 py-1 overflow-auto">
+      {errorMessage && <div className="error-message">{errorMessage}</div>}
+
       {messages.map((message: Message, index: number) => (
         <div
           key={index}
@@ -39,9 +47,9 @@ const ChatContent = ({ messages }: ChatContentProps) => {
             <Avatar />
           </div>
           <div
-            className={`px-2 w-fit py-3 flex flex-col bg-purple-500 rounded-lg text-white ${
-              message.isChatOwner ? "order-1 mr-2" : "order-2 ml-2"
-            }`}
+              className={`px-2 w-fit py-3 flex flex-col bg-purple-500 rounded-lg text-white ${
+                  message.isChatOwner ? "order-1 mr-2" : "order-2 ml-2"
+              }`}
           >
             <span className="text-xs text-gray-200">
               {message.sentBy}&nbsp;-&nbsp;
@@ -52,13 +60,13 @@ const ChatContent = ({ messages }: ChatContentProps) => {
             </span>
             <div className="text-s max-w-md break-words" id="targetItem">{message.text}</div>
             {message.isChatOwner && (
-              <button
-                type="button"
-                className="bg-transparent hover:bg-red-500 text-blue-200 font-semibold hover:text-black text-xs px-4 py-2  border border-black-300 hover:border-transparent rounded w-1/4 "
-                onClick={(el) => handleDecrypt(el, message)}
-              >
-                Decrypt
-              </button>
+                <button
+                    type="button"
+                    className="bg-transparent hover:bg-red-500 text-blue-200 font-semibold hover:text-black text-xs px-4 py-2  border border-black-300 hover:border-transparent rounded w-1/4 "
+                    onClick={(el) => handleDecrypt(el, message)}
+                >
+                  Decrypt
+                </button>
             )}
           </div>
         </div>
